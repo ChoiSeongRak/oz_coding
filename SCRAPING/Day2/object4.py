271229c4462d0409f7a86af50c3485ed35bb6bd6
ChoiSeongRak <kok12346@gmail.com>
2024-05-27 10:19:19 +0900

    day2 up

--- SCRAPING/Day2/object4.py
+++ SCRAPING/Day2/object4.py
@@ -32,20 +32,27 @@ for item in nav_items:
     if item.text == "멜론차트":
         item.click()
         break
+
+# 스크롤 5번
 for _ in range(5):
     driver.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.PAGE_DOWN)
     time.sleep(0.3)
-# 같은 클래스인 버튼 다 불러와서 onclick 속성이 hasMore2()인 요소 클릭
+
+# 더보기 버튼 onclick 속성이 hasMore2()인 더보기 클릭
 has_items = driver.find_elements(By.CSS_SELECTOR, "button#moreBtn.service_list_more.noline.sprite.hide")
 for item in has_items:
     if item.get_attribute("onclick") == "hasMore2();":
         item.click()
         break
+
+# 스크롤 5번
 time.sleep(0.5)
 for _ in range(5):
     driver.find_element(By.CSS_SELECTOR, "body").send_keys(Keys.PAGE_DOWN)
     time.sleep(0.3)
 time.sleep(1)
+
+# 데이터 가져와서 변수에 넣기
 html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
 songs = soup.select(".list_item")
@@ -53,13 +60,13 @@ songs = soup.select(".list_item")
 for song in songs:
     ranking_element = song.select_one(".ranking_num")
     titles = song.select_one(".title.ellipsis").text
-    singers = song.select_one(".name.ellipsis")
+    singers = song.select_one(".name.ellipsis").text
 
 
     if ranking_element:  # 요소가 존재하는지 확인
         print(f"순위:{ranking_element.text}")
         print(f"노래 제목:{titles.strip()}")
-        print(f"가수 이름:{singers.text}")
+        print(f"가수 이름:{singers}")
     else:
         pass
 
